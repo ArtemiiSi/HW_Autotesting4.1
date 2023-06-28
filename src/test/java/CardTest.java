ffac92967f6b8d844f8b51aff1769d3dd4a70d49
ArtemiiSi <107568989+ArtemiiSi@users.noreply.github.com>
2023-06-28 20:47:15 +0300

    add test

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,51 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardTest {
+    LocalDate today = LocalDate.now();
+    LocalDate newDate = today.plusDays(3);
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    String planningDate = generateDate(3);
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    @Test
+    void test() {
+
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $("[placeholder=\"Город\"]").setValue("Санкт-Петербург");
+        $("[data-test-id=date] input").sendKeys(formatter.format(newDate));
+        $("[data-test-id=\"name\"] input").setValue("Иванов Иван");
+        $("[data-test-id=\"phone\"] input").setValue("+78120000000");
+        $x("//span[contains(text(), 'Я соглашаюсь с условиями обработки')]").click();
+        $(".button__content").click();
+        $x("//div[text()= 'Успешно!']").should(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+    }
+    @Test
+    void incorrectPhoneNumber() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        $("[placeholder=\"Город\"]").setValue("Иркутск");
+        $("[data-test-id=date] input").sendKeys(formatter.format(newDate));
+        $("[data-test-id=\"name\"] input").setValue("Иванов Иван");
+        $("[data-test-id=\"phone\"] input").setValue("5432рпв098д");
+        $x("//span[contains(text(), 'Я соглашаюсь с условиями обработки')]").click();
+        $(".button__content").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79141234567."));
+    }
+
+
+}
